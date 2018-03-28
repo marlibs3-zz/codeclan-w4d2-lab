2d077e7d386b1b4a8ecde595e13b34c57ab403bc
martabeveridge <marta@beveridge.uk>
2018-03-28 18:31:39 +0100

    the app now uses params

--- app.rb
+++ app.rb
@@ -5,3 +5,7 @@ require 'sinatra/contrib/all' if development?
 get '/roll-die' do
   "Rolling the die... #{rand(1...7)}"
 end
+
+get '/name/:first/:last' do
+  return "Your name is #{params[:first]} #{params[:last]}"
+end
